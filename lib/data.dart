c4f46398d4141c98f086a9049ad4189e4325a344
saed.fathi@gmial.com <saed.fathi@gmail.com>
2022-01-20 12:33:03 +0330

    createclass studentData

---
+++ lib/data.dart
@@ -0,0 +1,12 @@
+class StudentData {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String course;
+  final int score;
+  final String createdAt;
+  final String updated_At;
+
+  StudentData(this.id, this.firstName, this.lastName, this.course, this.score,
+      this.createdAt, this.updated_At);
+}

81791dd54a92d5d8fbc9eb0006069589df3e1c6a
saed.fathi@gmial.com <saed.fathi@gmail.com>
2022-01-21 18:43:08 +0330

    HttpClient

--- lib/data.dart
+++ lib/data.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+
 class StudentData {
   final int id;
   final String firstName;
@@ -9,4 +11,30 @@ class StudentData {
 
   StudentData(this.id, this.firstName, this.lastName, this.course, this.score,
       this.createdAt, this.updated_At);
+  StudentData.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        firstName = json['first_name'],
+        lastName = json['last_name'],
+        course = json['course'],
+        score = json['score'],
+        createdAt = json['created_at'],
+        updated_At = json['updated_at'];
+}
+
+class HttpClient {
+  static Dio instance =
+      Dio(BaseOptions(baseUrl: 'http://expertdevelopers.ir/api/v1/'));
+}
+
+Future<List<StudentData>> getStudents() async {
+  final response = await HttpClient.instance.get('experts/student');
+  print(response.data);
+  final List<StudentData> students = [];
+  if (response.data is List<dynamic>) {
+    (response.data as List<dynamic>).forEach((element) {
+      students.add(StudentData.fromJson(element));
+    });
+  }
+  print(students.toString());
+  return students;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_api_send_recieve/data.dart';
 
 void main() {
+  getStudents();
   runApp(const MyApp());
 }
 
@@ -23,6 +25,114 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Android Expert'),
+        ),
+        body: FutureBuilder<List<StudentData>>(
+          future: getStudents(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData && snapshot != null) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return _Student(
+                    studentdata: snapshot.data![index],
+                  );
+                },
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ));
+  }
+}
+
+class _Student extends StatelessWidget {
+  final StudentData studentdata;
+  const _Student({Key? key, required this.studentdata}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 84,
+      padding: EdgeInsets.all(8),
+      margin: EdgeInsets.fromLTRB(8, 4, 8, 4),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(blurRadius: 10, color: Colors.black.withOpacity(0.05))
+          ]),
+      child: Row(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            width: 64,
+            height: 64,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Theme.of(context).colorScheme.primary.withOpacity(0.1)),
+            child: Text(
+              studentdata.firstName.characters.first,
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.w900,
+                  fontSize: 24),
+            ),
+          ),
+          SizedBox(
+            width: 8,
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(studentdata.firstName + ' ' + studentdata.lastName),
+                SizedBox(
+                  height: 8,
+                ),
+                Container(
+                    padding: EdgeInsets.fromLTRB(4, 2, 4, 2),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(2),
+                        color: Colors.grey.shade200),
+                    child: Text(
+                      studentdata.course,
+                      style: TextStyle(fontSize: 10),
+                    ))
+              ],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.bar_chart_rounded,
+                color: Colors.grey.shade400,
+              ),
+              Text(
+                studentdata.score.toString(),
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
